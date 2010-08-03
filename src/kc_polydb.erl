83c3838f03b5aa29b6a90c960e3ffaf67f9c55cc
UENISHI Kota <kuenishi+github@gmail.com>
2010-08-04 02:32:13 +0900

    creating basic(initial) files.

---
+++ src/kc_polydb.erl
@@ -0,0 +1,10 @@
+%%%-------------------------------------------------------------------
+%%% @author UENISHI Kota <kuenishi@gmail.com>
+%%% @copyright (C) 2010, UENISHI Kota
+%%% @doc
+%%%
+%%% @end
+%%% Created :  4 Aug 2010 by UENISHI Kota <kuenishi@gmail.com>
+%%%-------------------------------------------------------------------
+-module(kc_polydb).
+
